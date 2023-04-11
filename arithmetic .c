e84148677b734cf6f6f121c9c57fb9dac0ffb395
DakshSutariya <130463070+DakshSutariya@users.noreply.github.com>
2023-04-11 17:44:03 +0530

    Add files via upload

---
+++ arithmetic .c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+main()
+
+{
+	int a=10;
+	int b=42;
+	int c;
+	c=a+b;
+	printf("additon of A & B =%d\n",c);
+	
+	c=a-b;
+	printf("substraction of A & B = %d\n",c);
+	
+	c=a*b;
+	printf("multiplication of A & B = %d\n",c);
+	
+	c=a/b;
+	printf("division of A & B = %d\n",c);
+	
+	c=a%b;
+	printf("modulation of A & B = %d",c);
+}
+
